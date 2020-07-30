d7e3653f26f51fe3159ea0d9f692e4994330b4d4
GurkanwalKang <68421165+GurkanwalKang@users.noreply.github.com>
2020-07-30 18:33:29 +0530

    Add files via upload

---
+++ Final_Code.py
@@ -0,0 +1,35 @@
+import cv2
+import pytesseract
+import imutils
+import numpy as np
+
+pytesseract.pytesseract.tesseract_cmd = 'C:\Program Files\Tesseract-OCR\\tesseract.exe'
+image = cv2.imread('container_images/2.jpg')
+
+# Display the original image
+cv2.imshow("Original Image", image)
+cv2.waitKey(0)
+
+# RGB to Gray scale conversion
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+# Noise removal with iterative bilateral filter(removes noise while preserving edges)
+gray = cv2.bilateralFilter(gray, 11, 17, 17)
+
+# ROI Selector
+r = cv2.selectROI( gray )
+width_start, height_start, width_end, height_end = r
+cropped_img = gray[height_start: height_start + height_end, width_start: width_start + width_end]
+cv2.imwrite( 'CroppedImage.jpg', cropped_img )  # Saving Cropped Image
+cv2.imshow('Cropped Image', cropped_img)
+cv2.waitKey(0)  
+
+#Image Inverted
+imagem = cv2.bitwise_not(cropped_img)
+
+#Bilateral Filtering
+gray_new = cv2.bilateralFilter(imagem, 11, 17, 17)
+print( " Container Number = " , pytesseract.image_to_string( gray_new )) # Try 1 (BEST RESULT IN MOST CASES)
+
+cv2.imshow("Bilateral Filter", gray_new )
+cv2.waitKey(0)
